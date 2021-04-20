bce786188669ff5c2a60e73abfec750483f47a00
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-04-20 11:00:41 +0100

    pip install from requirements.txt verbosely

--- scripts/run_python_tests.sh
+++ scripts/run_python_tests.sh
@@ -17,7 +17,7 @@ echo "Coverage directory is ${coverageDirectory}"
 
 if [[ -f "${testsDirectory}/requirements.txt" ]]; then
   echo "Tests dependencies found, installing..."
-  python3 -m pip install --upgrade -r "${testsDirectory}/requirements.txt"
+  python3 -m pip install --verbose --upgrade -r "${testsDirectory}/requirements.txt"
 fi
 
 if [[ -f "${targetDirectory}/Pipfile" ]]; then
